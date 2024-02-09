34564f67bf5611ccdf6b3d938fab8505dcd44547
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-02-09 21:32:42 +0700

    update again

--- database/models/index.js
+++ database/models/index.js
@@ -1,11 +1,12 @@
 "use strict";
-require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
+const dotenv = require('dotenv');
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
+dotenv.config();
 const config = require(__dirname + "/../../config/config.json")[env];
 const db = {};
 
