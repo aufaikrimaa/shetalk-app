a57609f1e078ec635f4fe60e29f555f3d1197056
Aufa Ikrimah <aufaikrimah@gmail.com>
2024-02-12 14:36:16 +0700

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = process.env.APP_PORT || 3000;
+const port = process.env.APP_PORT || 8000;
 const cors = require("cors");
 const parser = require("body-parser");
 const mainRouter = require("./routes");
